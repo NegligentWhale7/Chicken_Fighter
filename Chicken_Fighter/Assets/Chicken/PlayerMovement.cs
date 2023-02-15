fe8601d36600959a9656b0242f71b0c79debcf20
Alam Armas <carlosalam33@hotmail.com>
2023-02-15 16:51:05 -0600

    swipe

--- Chicken_Fighter/Assets/Chicken/PlayerMovement.cs
+++ Chicken_Fighter/Assets/Chicken/PlayerMovement.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GG.Infrastructure.Utils.Swipe;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private SwipeListener swipeListener;
 
+    private void OnEnable()
+    {
+        swipeListener.OnSwipe.AddListener(OnSwipe);
+    }
+    private void OnSwipe(string swipe)
+    {
+        Debug.Log(swipe);
+    }
+    private void OnDisable()
+    {
+        swipeListener.OnSwipe.RemoveListener(OnSwipe);
+    }
 }
 
